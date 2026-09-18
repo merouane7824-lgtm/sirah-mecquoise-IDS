70c64440c49dfce0a78a7a077ece06b7170c5cf5
merouane7824-lgtm <merouane7824@gmail.com>
2026-09-18 17:56:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,29 @@
-const CACHE_NAME = 'sirah-mecquoise-v1';
-const ASSETS = ['./', './index.html', './manifest.webmanifest', './sw.js'];
+const CACHE = "sirah-mecquoise-v2";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js", "./icon-512.png"];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).then(response => {
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).then(response => {
         const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
         return response;
-      }).catch(() => caches.match('./index.html'));
-    })
+      }).catch(() => caches.match("./index.html"))
+    )
   );
 });
